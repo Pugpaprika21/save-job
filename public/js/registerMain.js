36eef5d61b311addb0a087f0bb1595504fe44d0c
Pugpaprika21 <pugpaprika21@gmail.com>
2023-06-09 07:19:47 -0700

    e

--- public/js/registerMain.js
+++ public/js/registerMain.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const registerURL = document.getElementById("register-url").value;
+    const registerMain = document.getElementById("register-main");
 
+    registerMain.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        let username = document.getElementById("username").value;
+        let userpass = document.getElementById("userpass").value;
+        let userPhone = document.getElementById("userPhone").value;
+        let userEmail = document.getElementById("userEmail").value;
+    });
 });
