adb94969eb4645be4a829f8297a532b9550a825c
Pugpaprika21 <pugpaprika21@gmail.com>
2023-06-10 06:16:37 -0700

    e

--- pages/home.php
+++ pages/home.php
@@ -6,8 +6,6 @@ if (empty($_SESSION['user_data'])) {
     redirect_to("../pages/login.php", array('user_name' => 'N', 'user_pass' => 'N'));
 }
 
-$main_manu = db_select("select count(*) as count_manu from manu_main_system_tb");
-
 ?>
 
 <?php require __DIR__ . "../../view/layout/header.php"; ?>
@@ -90,14 +88,19 @@ $main_manu = db_select("select count(*) as count_manu from manu_main_system_tb")
 <!-- card body -->
 
 <div class="card-content-main" style="margin-top: 40px;">
+    <?php
+    // get manu
+    $num_main = db_select("select count(*) as count_manu from manu_main_system_tb where ms_status = 'Y'");
+    
+    ?>
     <div class="container-fluid">
         <div class="card">
             <div class="card-body shadow-sm">
                 <div class="card-content-sub">
                     <div class="row row-cols-1 row-cols-md-3 g-4">
-                        <?php if ($main_manu['count_manu'] > 0) : ?>
+                        <?php if ($num_main['count_manu'] > 0) : ?>
 
-                        <?php elseif ($main_manu['count_manu'] == 0) : ?>
+                        <?php elseif ($num_main['count_manu'] == 0) : ?>
                             <div class="col">
                                 <div class="card h-100" id="card-manu-">
                                     <div class="card-body shadow-sm rounded">
@@ -105,7 +108,7 @@ $main_manu = db_select("select count(*) as count_manu from manu_main_system_tb")
                                             <div class="text-center mt-4 mb-4">
                                                 <img src="../upload/image/home_manu.png" class="rounded" alt="..." style="width: 20%; height: 85%;">
                                             </div>
-                                            <h5 class="card-title">เมนูหลัก / <?= APP_NAME ?></h5>
+                                            <h5 class="card-title">เมนูหลัก / ระบบ<?= APP_NAME ?></h5>
                                             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                         </a>
                                     </div>
--- view/@@templates/main_card_body_content.php
+++ view/@@templates/main_card_body_content.php
@@ -1,49 +1,33 @@
-<div class="card-content-main" style="margin-top: 20px;">
-    <div class="container-fluid shadow-sm">
+<div class="card-content-main" style="margin-top: 40px;">
+    <?php
+    // get manu
+    $main_manu = db_select("select count(*) as count_manu from manu_main_system_tb");
+    
+    ?>
+    <div class="container-fluid">
         <div class="card">
-            <div class="card-body">
-                <!--  -->
+            <div class="card-body shadow-sm">
                 <div class="card-content-sub">
                     <div class="row row-cols-1 row-cols-md-3 g-4">
-                        <div class="col">
-                            <div class="card h-100">
-                                <img src="..." class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <?php if ($main_manu['count_manu'] > 0) : ?>
+
+                        <?php elseif ($main_manu['count_manu'] == 0) : ?>
+                            <div class="col">
+                                <div class="card h-100" id="card-manu-">
+                                    <div class="card-body shadow-sm rounded">
+                                        <a href="http://" class="text-decoration-none">
+                                            <div class="text-center mt-4 mb-4">
+                                                <img src="../upload/image/home_manu.png" class="rounded" alt="..." style="width: 20%; height: 85%;">
+                                            </div>
+                                            <h5 class="card-title">เมนูหลัก / ระบบ<?= APP_NAME ?></h5>
+                                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col">
-                            <div class="card h-100">
-                                <img src="..." class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a short card.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="card h-100">
-                                <img src="..." class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="card h-100">
-                                <img src="..." class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                </div>
-                            </div>
-                        </div>
+                        <?php endif; ?>
                     </div>
                 </div>
-                <!--  -->
             </div>
         </div>
     </div>
