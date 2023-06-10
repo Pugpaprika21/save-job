23803e64cb6d6e970044ab2615037dae63246b09
Pugpaprika21 <pugpaprika21@gmail.com>
2023-06-10 06:09:30 -0700

    e

--- pages/home.php
+++ pages/home.php
@@ -90,9 +90,9 @@ $main_manu = db_select("select count(*) as count_manu from manu_main_system_tb")
 <!-- card body -->
 
 <div class="card-content-main" style="margin-top: 40px;">
-    <div class="container-fluid shadow-sm">
+    <div class="container-fluid">
         <div class="card">
-            <div class="card-body">
+            <div class="card-body shadow-sm">
                 <div class="card-content-sub">
                     <div class="row row-cols-1 row-cols-md-3 g-4">
                         <?php if ($main_manu['count_manu'] > 0) : ?>
@@ -102,10 +102,10 @@ $main_manu = db_select("select count(*) as count_manu from manu_main_system_tb")
                                 <div class="card h-100" id="card-manu-">
                                     <div class="card-body shadow-sm rounded">
                                         <a href="http://" class="text-decoration-none">
-                                            <div class="text-center mt-2 mb-2">
+                                            <div class="text-center mt-4 mb-4">
                                                 <img src="../upload/image/home_manu.png" class="rounded" alt="..." style="width: 20%; height: 85%;">
                                             </div>
-                                            <h5 class="card-title"><?= APP_NAME ?></h5>
+                                            <h5 class="card-title">เมนูหลัก / <?= APP_NAME ?></h5>
                                             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                         </a>
                                     </div>
