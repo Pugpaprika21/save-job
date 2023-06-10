814d09476da6a2d3770b932fb26b92e1304b031a
Pugpaprika21 <pugpaprika21@gmail.com>
2023-06-09 21:31:57 -0700

    e

--- pages/home.php
+++ pages/home.php
@@ -1,6 +1,13 @@
-<?php require __DIR__ . "../../src/include/include.php"; ?>
+<?php 
+
+require __DIR__ . "../../src/include/include.php"; 
+
+echo_r($_SESSION);
+
+?>
 
 <?php require __DIR__ . "../../view/layout/header.php"; ?>
+
 
 
 <?php require __DIR__ . "../../view/layout/footer.php"; ?>
